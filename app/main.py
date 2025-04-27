79ae5ca925d0395cb85d77e43fde2b18bc65b50e
felixettl187 <fe871ett@htwg-konstanz.de>
2025-04-27 16:13:55 +0200

    test double push

--- app/main.py
+++ app/main.py
@@ -30,3 +30,7 @@ def extract_text_from_pdf(file_bytes: bytes) -> str:
             text += page.extractText()
         print(text)
         return text
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- app/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Starte Easy Learn Server auf http://localhost:8000 ..."
-uvicorn main:app --reload --host 0.0.0.0 --port 8000
